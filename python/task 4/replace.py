d2923785516856f832e2091eaffa156f58a20bd5
Anjal <anjalkoranath@gmail.com>
2025-08-28 14:54:58 +0530

    adding task 4 containg replace

---
+++ python/task 4/replace.py
@@ -0,0 +1,17 @@
+str ="A man, a plan, a canal,panama"
+print('-'.join(str))
+
+
+print("-"*50)
+
+
+str1="A man, a plan, a canal,panama"
+list1=str1.split(",")
+print(list1)
+
+
+print("-"*50)
+
+
+str2="A man, a plan, a canal,panama"
+print(str2.replace(","," "))
