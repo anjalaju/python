3c1bc80c1ad10e168f3a0d88001eb7c3b9e71975
Anjal <anjalkoranath@gmail.com>
2025-09-21 19:36:04 +0530

    adding file handling,os,tkinter

---
+++ python/os.py
@@ -0,0 +1,31 @@
+# import os 
+# print(os.listdir())
+# print(os.getcwd())
+# # os.removedirs("scope india/scope")
+
+# # print(os.getcwd)
+# # os.remove("a.py")
+# # os.rename("C:\\Users\\ANJAL\\OneDrive\\Desktop\\python\\scope india\\scope\\a.py","b.py")
+# print(os.path.exists("C:\\Users\\ANJAL\\OneDrive\\Desktop\\python\\b.py"))
+# # os.makedirs("Anjalk")
+# # print(dir(os))
+
+# # from datetime import *
+# # print("Current date and time:",datetime.now())
+
+
+# # import csv
+
+# from tkinter import *
+# main=Tk()
+# main.title("my first")
+# main.mainloop()
+
+import tkinter as tk
+main=tk.Tk()
+main.title("my first")
+main.geometry("400x300")
+main.mainloop()
+
+
+
