4fbadcb2c2fa847275e7404c7d44040d0c31ecc5
Anjal <anjalkoranath@gmail.com>
2025-09-22 16:09:43 +0530

    adding database programs

---
+++ python/db.py
@@ -0,0 +1,42 @@
+import sqlite3
+connection=sqlite3.connect("Anjal.db")
+cursor=connection.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name VARCHAR(20)NOT NULL,
+               age INTEGER,
+               gender VARCHAR(10),
+               email VARCHAR(50)UNIQUE,
+               phone_no VARCHAR(15)UNIQUE,
+               address TEXT)
+               """)
+
+# cursor.execute("""  
+# INSERT INTO student(name,age,gender,email,phone_no,address)VALUES
+               
+#                ("haris",21,"male","haris@gmail.com","8934678900","abc(H)karkkidamkunnu po")
+                  
+#  """)
+
+# cursor.execute(""" 
+#                 UPDATE student 
+#                 SET email="adarsh@gmail.com",name="adarsh"WHERE name="Anjal"      
+               
+#                """)
+
+
+# cursor.execute("SELECT * FROM student")
+# rows=cursor.fetchall()
+# for row  in rows:
+#     print(row)
+
+
+
+# cursor.execute("""
+# DELETE FROM student WHERE name="An"
+#  """)
+
+
+connection.commit()
+connection.close()
+
